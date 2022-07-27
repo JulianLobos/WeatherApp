0054e6ae5ab91d084495c9212f4d115896a3711a
julianLobos <julianign@gmail.com>
2022-07-26 21:28:34 -0300

    update

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='navbar'>
+            <p className='navTitle'>WeatherApp</p>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
